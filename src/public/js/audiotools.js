8f9f8161698aa1b065d63eae90946c6db919b26a
Kyle Mattimore <kmattimo@umich.edu>
2016-04-16 15:03:21 -0500

    timing

--- src/public/js/audiotools.js
+++ src/public/js/audiotools.js
@@ -1,13 +1,28 @@
 "use strict";
-
 //utility classes
 
+//call the instances of these functions (window.audioTools not Audiotools)
+//not the classes themselves
 var AudioTools = function() {
   
   this.test = function() {
     console.log('la la la');
   };
 
-}
+};
+
+
+//clock.time() = number of seconds elapsed
+var Clock = function() {
+//https://gist.github.com/electricg/4372563
+  this.now = function() { return (new Date()).getTime(); }; 
+  var startAt = 0;
+  this.start = function() { startAt	= this.now(); };
+  this.time = function() { return Math.floor((startAt ? this.now() - startAt : 0)/ 1000 ); };
+  
+  this.start();
+};
+
 
 window.audioTools = new AudioTools();
+window.clock = new Clock();
