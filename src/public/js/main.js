942f006d2fdbd76a7a9a5a0fc3896dcd3b3cf1cd
Kyle Mattimore <kmattimo@umich.edu>
2016-04-16 19:51:50 -0500

    moved around

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,8 +1,5 @@
-window.audioTools = require("audioTools");
+window.audioTools = require("./audioTools");
 console.log ("this is a test: " + $(window).width()) ;
 
 audioTools.test();
-
-
-// require('./drummachine.js');
 
