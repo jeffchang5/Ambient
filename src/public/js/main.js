22b66df8bc41845db6dcb374cbf4a699ac708caa
Jeffrey Chang <jeffreychang@wireles-guest-16-40-23.uchicago.edu>
2016-04-16 19:43:27 -0500

    didn't do much

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,6 +1,64 @@
 // window.audioTools = require("audioTools");
-console.log ("this is a test: " + $(window).width()) ;
+'use strict';
+
+$('document').ready(function() {
+    var array_of_files = [];
+    var array_of_urls = [];
+
+
+    $.ajax({
+        url: "/public/samples/_file_list.txt",
+        success: function(data) {
+            array_of_files = data.split('\n');
+            for (var i in array_of_files) {
+                array_of_urls.push("/public/samples/" + array_of_files[i]);
+
+            }
+            console.log(array_of_urls);
+
+        }
+
+    });
+});
+
+
+
+            //$("#play_button").show();
 
 audioTools.test();
 
 
+// // window.audioTools = require("audioTools");
+// 'use strict';
+// window.sample_array = [];
+// $('document').ready(function() {
+//     var array_of_files = [];
+//
+//     $.ajax({
+//         url: "/public/samples/_file_list.txt",
+//         success: function(data) {
+//             array_of_files = data.split('\n');
+//             for (var i in array_of_files) {
+//
+//                 $.ajax({
+//                     url: "/public/samples/" + array_of_files[i],
+//                     success: function (data) {
+//                         sample_array.push(data);
+//
+//
+//
+//                     }
+//
+//
+//                     //$("#play_button").show();
+//                 });
+//
+//             }
+//
+//         }
+//
+//     });
+//     console.log(sample_array);
+//
+//
+// });
