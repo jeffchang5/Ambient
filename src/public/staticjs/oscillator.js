a87cc44b0928af5147e342a20e00594a98fdea9b
Edwin Wu <edwinwu2017@u.northwestern.edu>
2016-04-17 06:03:37 -0500

    messed around didn't do much

--- src/public/staticjs/oscillator.js
+++ src/public/staticjs/oscillator.js
@@ -1,7 +1,8 @@
 var midi = new Array(127);
 var a = 440;    	
 var lowNote = 261.63; // C4
-  var highNote = 493.88; // B4
+var highNote = 493.88; // B4
+var context = null;
 
 
 // console.log(midi);
@@ -34,198 +35,203 @@ var chordprogression3 = [major5, major5, major5, major5];
 var majorminor3 = [true, true, true, true];
 var chordprogression4 = [minor5, minor5, major5, major5];
 var majorminor4 = [false, false, true, true];
+
 var chords = 
     [[startinvals1, chordprogression1, majorminor1], 
     [startinvals2, chordprogression2, majorminor2],
     [startinvals3, chordprogression3, majorminor3],
-    [startinvals4, chordprogression4, majorminor4]]
+    [startinvals4, chordprogression4, majorminor4]];
 
-
+var musicstop = false;
 
 function coolStuff() {
   // console.log('does this work?');
 }
 
-function makeSound(){
-        window.director.start;
-        var context = window.metadata.audioContext;
+function stop(){
+    musicstop = true;
+}
+
+function makeSound(context){ 
+        // window.director.start;
         
+        // postMessage(i);
         
-        for (repeat = 0; repeat < 5; repeat ++){
-            var freqtotal = [];
-            var curchordprog = chords[Math.floor(Math.random()*chords.length)];
-            var curmajorminor = curchordprog[2];
+    var freqtotal = [];
+    var curchordprog = chords[Math.floor(Math.random()*chords.length)];
+    var curmajorminor = curchordprog[2];
 
-            // var startingnnote = Math.floor(Math.random()*12);
-            // curmajorminor.push();
-            // console.log("what");
-            // console.log(curchordprog);
-            for (k = 0; k < curchordprog[0].length; k ++){
-                tempval = []
-                var startingnnote = Math.floor(Math.random()*12);
-                curchordprog[0][k] +=startingnnote;
-                for (k2 = 0; k2 < curchordprog[1][k].length; k2 ++){
-                    
-                    // curchordprog[0][k][k2] += startingnnote;
-                    // console.log(midi[curchordprog[1][k][k2]+48+curchordprog[0][k]]);
-                    tempval.push(midi[curchordprog[1][k][k2]+32+curchordprog[0][k]]);
-                }
-                freqtotal.push(tempval);   
-            }
-            console.log(freqtotal);
+    // var startingnnote = Math.floor(Math.random()*12);
+    // curmajorminor.push();
+    // console.log("what");
+    // console.log(curchordprog);
+    for (k = 0; k < curchordprog[0].length; k ++){
+        tempval = []
+        var startingnnote = Math.floor(Math.random()*12);
+        curchordprog[0][k] +=startingnnote;
+        for (k2 = 0; k2 < curchordprog[1][k].length; k2 ++){
             
-            // var freqtotal2 = [];
-            // for (k = 0; k < curchordprog[0].length; k ++){
-            //     tempval = [];
-            //     for (k2 = 0; k2 < notes[k].length; k2++){
-            //         tempval.push(midi[notes[k][k2]+60+curchordprog[0][k]]);
-            //     }
-            //     freqtotal2.push(tempval);
-            // }
-            
-            // var minor7  = [0, 3, 7, 10];
-            // var freqs = [midi[67], midi[64], midi[60], midi[72]];
-            // var freqs2 = [midi[60], midi[64], midi[71], midi[74]];
-            // var freqs3 = [midi[60], midi[63], midi[67], midi[70]];
-            // var freqtotal = [freqs, freqs2, freqs3];
-
-            
-            
-            repeatval = 2;
-            checker = true;
-            cuval = 0;
-            quickness = 2.5;
-            nextMeasureTime = 0;
-            j=0;
-
-            while(j < 24){
-                while(nextMeasureTime < context.currentTime + quickness){
-                    nextMeasureTime += 2.5;
-
-                
-                
-                var oscs = [];
-                
-                if (checker){
-                        curmajmin = (j/2)%curmajorminor.length;
-                        curfreq = freqtotal[(j/2)%freqtotal.length];
-
-                        // console.log((j/2)%freqtotal.length);       
-                        checker = false;
-                        cuval ++;
-                }
-                else if (cuval + 1 < repeatval){
-                        cuval ++;
-                }
-                else{
-                        // console.log("yoyoyo");
-                        checker = true;
-                        cuval = 0;
-                }
-
-                // console.log(curfr eq);
-                for (x = 0; x < curfreq.length; x ++){
-                    var nodes = {};
-                    nodes.filter = context.createBiquadFilter();
-                    nodes.convolver = context.createConvolver();
-                    nodes.volume = context.createGain();
-                    // var gainNode = context.createGain();
-                    var oscillator = context.createOscillator();
-                    oscillator.frequency.value = curfreq[x];
-                    // oscillator..value = 50volume
-                    oscillator.type = "sine"
-                    // var biquadFilter = context.createBiquadFilter();
-                    oscillator.connect(nodes.filter);
-                    // oscillator.connect(nodes.convolver);
-                    nodes.filter.connect(nodes.volume);
-                    nodes.volume.gain.value = 0.10                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ;
-                    // biquadFilter.connect(gainNode);
-                    // nodes.filter.type = "lowshelf";
-                    // biquadFilter.frequency.value = 100;
-                    // biquadFilter.gain.value = 30;                 
-                    nodes.volume.connect(context.destination);
-                    oscillator.start((quickness*(repeat * 24 +j)).toFixed(9));
-                    // gainNode = context.createGainNode();
-                    // gainNode.connect(context.destination);
-                    // oscillator.connect(gainNode);
-                    // Connect the oscillator to our speakers
-                    oscillator.stop((quickness*(repeat * 24 +j+.7)).toFixed(9));
-                    oscs.push(oscillator);
-                    // break;
-                }
-                var compressor = context.createDynamicsCompressor();
-                compressor.threshold.value = -20;
-                compressor.knee.value = 15;
-                compressor.ratio.value = 12;
-                compressor.reduction.value = -20;
-                compressor.attack.value = 0.001;
-                compressor.release.value = 0.001;
-                for (v = 0; v < 4; v ++){
-                    // break;
-                    var distortion = context.createWaveShaper();
-
-
-
-                    distortion.curve = makeDistortionCurve(0);
-                    distortion.oversample = '4x';
-                    var nodes = {};
-                    nodes.filter = context.createBiquadFilter();
-                    nodes.convolver = context.createConvolver();
-                    nodes.volume = context.createGain();
-                    var gainNode = context.createGain();
-                    var oscillator = context.createOscillator();
-                    // console.log(Math.floor(Math.random()*curmajorminor.length));
-                    var tempval = returnNotes(curmajorminor[curmajmin], curchordprog[0][curmajmin]);
-                    // console.log("whatwhatwhat");
-                    // console.log(tempval);
-                    oscillator.frequency.value = tempval;
-
-                    // oscillator..value = 50;
-                    oscillator.type = "sine";
-                    // var biquadFilter = context.createBiquadFilter();
-                    oscillator.connect(nodes.filter);
-                    oscillator.connect(compressor);
-                    oscillator.connect(distortion);
-                    nodes.filter.type = "highpass";
-                    nodes.filter.frequency.value = oscillator.frequency.value+5;
-                    oscillator.connect(nodes.convolver);
-                    oscillator.connect(nodes.filter);
-                    nodes.volume.gain.value = 0.40;
-                    nodes.filter.connect(nodes.volume);
-                    // biquadFilter.connect(gainNode);
-                    // biquadFilter.type = "lowshelf";
-                    // biquadFilter.frequency.value = 100;
-                    // biquadFilter.gain.value = 30;                 
-                    nodes.volume.connect(context.destination);
-                    
-                    var scheduledTime = quickness*((repeat * 24) +(4*j+v)/4).toFixed(5);
-                    // console.log('time: ' + scheduledTime);
-                    oscillator.start(scheduledTime);
-                    
-                    // console.log(scheduledTime);
-                    window.sequencer.play(scheduledTime);
-                    
-                    // gainNode = context.createGainNode();
-                    // gainNode.connect(context.destination);
-                    // oscillator.connect(gainNode);
-                    // Connect the oscillator to our speakers
-                    oscillator.stop(quickness*((repeat * 24) +(4*j+v+0.7)/4).toFixed(5));
-
-                    
-                    // break;
-                }
-                j ++;
-            }
-                // console.log(freqs);
-                // console.log(oscs);
-               
-                    
-                    // for (i = 0; i < oscs.length; i ++){
-                    //         oscs[i].stop((j+.5)/2);
-                            
-                    // }
-            }
+            // curchordprog[0][k][k2] += startingnnote;
+            // console.log(midi[curchordprog[1][k][k2]+48+curchordprog[0][k]]);
+            tempval.push(midi[curchordprog[1][k][k2]+32+curchordprog[0][k]]);
         }
+        freqtotal.push(tempval);   
+    }
+    console.log(freqtotal);
+    
+    // var freqtotal2 = [];
+    // for (k = 0; k < curchordprog[0].length; k ++){
+    //     tempval = [];
+    //     for (k2 = 0; k2 < notes[k].length; k2++){
+    //         tempval.push(midi[notes[k][k2]+60+curchordprog[0][k]]);
+    //     }
+    //     freqtotal2.push(tempval);
+    // }
+    
+    // var minor7  = [0, 3, 7, 10];
+    // var freqs = [midi[67], midi[64], midi[60], midi[72]];
+    // var freqs2 = [midi[60], midi[64], midi[71], midi[74]];
+    // var freqs3 = [midi[60], midi[63], midi[67], midi[70]];
+    // var freqtotal = [freqs, freqs2, freqs3];
+
+    repeat = 0;
+    
+    repeatval = 2;
+    checker = true;
+    cuval = 0;
+    quickness = 2.5;
+    nextMeasureTime = 0;
+    j=0;
+
+    while(j < 24){
+
+        if (musicstop){
+            break;
+        }
+
+
+        while(nextMeasureTime < context.currentTime + quickness){
+            nextMeasureTime += quickness;
+
+            var oscs = [];
+            
+            if (checker){
+                    curmajmin = (j/2)%curmajorminor.length;
+                    curfreq = freqtotal[(j/2)%freqtotal.length];
+
+                    // console.log((j/2)%freqtotal.length);       
+                    checker = false;
+                    cuval ++;
+            }
+            else if (cuval + 1 < repeatval){
+                    cuval ++;
+            }
+            else{
+                    // console.log("yoyoyo");
+                    checker = true;
+                    cuval = 0;
+            }
+
+            // console.log(curfr eq);
+            for (x = 0; x < curfreq.length; x ++){
+                var nodes = {};
+                nodes.filter = context.createBiquadFilter();
+                nodes.convolver = context.createConvolver();
+                nodes.volume = context.createGain();
+                // var gainNode = context.createGain();
+                var oscillator = context.createOscillator();
+                oscillator.frequency.value = curfreq[x];
+                // oscillator..value = 50volume
+                oscillator.type = "sine"
+                // var biquadFilter = context.createBiquadFilter();
+                oscillator.connect(nodes.filter);
+                // oscillator.connect(nodes.convolver);
+                nodes.filter.connect(nodes.volume);
+                nodes.volume.gain.value = 0.10                                                                                                                                                                                                                                                                                                                                                                                                                                                                       ;
+                // biquadFilter.connect(gainNode);
+                // nodes.filter.type = "lowshelf";
+                // biquadFilter.frequency.value = 100;
+                // biquadFilter.gain.value = 30;                 
+                nodes.volume.connect(context.destination);
+                oscillator.start((quickness*(repeat * 24 +j)).toFixed(9));
+                // gainNode = context.createGainNode();
+                // gainNode.connect(context.destination);
+                // oscillator.connect(gainNode);
+                // Connect the oscillator to our speakers
+                oscillator.stop((quickness*(repeat * 24 +j+.7)).toFixed(9));
+                oscs.push(oscillator);
+                // break;
+            }
+            var compressor = context.createDynamicsCompressor();
+            compressor.threshold.value = -20;
+            compressor.knee.value = 15;
+            compressor.ratio.value = 12;
+            compressor.reduction.value = -20;
+            compressor.attack.value = 0.001;
+            compressor.release.value = 0.001;
+            for (v = 0; v < 4; v ++){
+                // break;
+                var distortion = context.createWaveShaper();
+                distortion.curve = makeDistortionCurve(100);
+                distortion.oversample = '4x';
+                var nodes = {};
+                nodes.filter = context.createBiquadFilter();
+                nodes.convolver = context.createConvolver();
+                nodes.volume = context.createGain();
+                nodes.panner = context.createPanner();
+                // var gainNode = context.createGain();
+                var oscillator = context.createOscillator();
+                // console.log(Math.floor(Math.random()*curmajorminor.length));
+                var tempval = returnNotes(curmajorminor[curmajmin], curchordprog[0][curmajmin]);
+                // console.log("whatwhatwhat");
+                // console.log(tempval);
+                oscillator.frequency.value = tempval;
+                nodes.panner.setVelocity(0, 15, 15);
+                // oscillator..value = 50;
+                oscillator.type = "sine";
+                // var biquadFilter = context.createBiquadFilter();
+                oscillator.connect(nodes.filter);
+                oscillator.connect(compressor);
+                oscillator.connect(distortion);
+                nodes.filter.type = "highpass";
+                nodes.filter.frequency.value = oscillator.frequency.value+5;
+                oscillator.connect(nodes.convolver);
+                oscillator.connect(nodes.filter);
+                oscillator.connect(nodes.panner);
+
+                nodes.volume.gain.value = 0.40;
+                nodes.filter.connect(nodes.volume);
+                // biquadFilter.connect(gainNode);
+                // biquadFilter.type = "lowshelf";
+                // biquadFilter.frequency.value = 100;
+                // biquadFilter.gain.value = 30;                 
+                nodes.volume.connect(context.destination);
+                
+                var scheduledTime = quickness*((repeat * 24) +(4*j+v)/4).toFixed(5);
+                // console.log('time: ' + scheduledTime);
+                oscillator.start(scheduledTime);
+                // console.log(scheduledTime);
+                window.sequencer.play(scheduledTime);
+                // gainNode = context.createGainNode();
+                // gainNode.connect(context.destination);
+                // oscillator.connect(gainNode);
+                // Connect the oscillator to our speakers
+                oscillator.stop(quickness*((repeat * 24) +(4*j+v+0.7)/4).toFixed(5));
+                // break;
+            }
+            j ++;
+        }
+        
+        // console.log(freqs);
+        // console.log(oscs);
+       
+            
+            // for (i = 0; i < oscs.length; i ++){
+            //         oscs[i].stop((j+.5)/2);
+                    
+            // }
+            }
+        
             
         
         
@@ -283,3 +289,33 @@ function makeDistortionCurve(amount) {
   }
   return curve;
 }
+
+// function init(){
+//     context = window.metadata.audioContext;
+
+
+//     timerWorker = new Worker("public/staticjs/oscillatorworker.js");
+
+//     timerWorker.onmessage = function(e) {
+//         if (e.data == "tick") {
+//             // console.log("tick!");
+//             makeSound(context);
+//         }
+//         else
+//             console.log("message: " + e.data);
+//     };
+//     timerWorker.postMessage({"interval":'start'});
+// }
+function async(fn, callback){
+    setTimeout(function(){
+        fn();
+        callback();
+    }, 0);
+}
+
+function run(){
+    async(makeSound(window.metadata.audioContext), function(){
+        console.log("worked")
+    });
+}
+
