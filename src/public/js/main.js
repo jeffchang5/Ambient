4eec482626bef26a07d56bfad514c57935bb7941
Jeffrey Chang <jeffreychang@wireles-guest-16-40-23.uchicago.edu>
2016-04-16 23:30:59 -0500

    Jeffrey

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,27 +1,8 @@
-console.log ("this is a test: " + $(window).width()) ;
-
-
 'use strict';
 
-$('document').ready(function() {
-    var array_of_files = [];
-    var array_of_urls = [];
 
 
-    $.ajax({
-        url: "/public/samples/_file_list.txt",
-        success: function(data) {
-            array_of_files = data.split('\n');
-            for (var i in array_of_files) {
-                array_of_urls.push("/public/samples/" + array_of_files[i]);
 
-            }
-            console.log(array_of_urls);
-
-        }
-
-    });
-});
 
 
 
