933381910fb4efeea7d117f4c6caf7c1f2575dd9
Edwin Wu <edwinwu2017@u.northwestern.edu>
2016-04-17 01:10:00 -0500

    chord progression pretty

--- src/public/staticjs/oscillator.js
+++ src/public/staticjs/oscillator.js
@@ -3,12 +3,42 @@ var a = 440;
 var lowNote = 261.63; // C4
   var highNote = 493.88; // B4
 
+
 console.log(midi);
 for (x = 0.0000; x < 127; x++){
 	midi[x] = (a/32) * (Math.pow(2, (x-9)/12));
 	console.log(midi[x]);
 	console.log(x);
 }
+var majorscale = [0, 2, 4, 5, 7, 9, 11, 12];
+var minorscale = [0, 2, 3, 5, 7, 8, 10, 12];
+var dom7 = [0, 4, 7, 10];
+var dim7 = [0, 3, 6, 9];
+var major7 = [0, 4, 7, 11];
+var minor7 = [0, 3, 7, 10];
+var major5 = [0, 4, 7];
+var minor5 = [0, 3, 7];
+var minor5d = [0, 3, 6];
+var augment = [0, 4, 8];
+var chord2 = [0, 2, 7];
+var startinvals1 = [4, -3, 8, -3, -1, 4];
+var startinvals2 = [0, 7, 9, 5]
+var startinvals3 = [0, 4, 5, 0]
+var startinvals4 = [9, 2, 7, 0]
+var startinvals5 = [2, 11, 7, 2, 9]
+var chordprogression1 = [major5, major5, minor5, major5, major5, major5];
+var majorminor1 = [4, true, true, false, true, true, true];
+var chordprogression2 = [major5, major5, minor5, major5];
+var majorminor2 = [0, true, true, false, true];
+var chordprogression3 = [major5, major5, major5, major5];
+var majorminor3 = [0, true, true, true, true];
+var chordprogression4 = [minor5, minor5, major5, major5];
+var majorminor4 = [9, false, false, true, true];
+var chords = 
+    [[startinvals1, chordprogression1, majorminor1], 
+    [startinvals2, chordprogression2, majorminor2],
+    [startinvals3, chordprogression3, majorminor3],
+    [startinvals4, chordprogression4, majorminor4]]
 
 
 
@@ -18,52 +48,38 @@ function coolStuff() {
 
 function makeSound(){
         var context = new AudioContext();
-        var dom7 = [0, 4, 7, 10];
-        var dim7 = [0, 3, 6, 9];
-        var major7 = [0, 4, 7, 11];
-        var minor7 = [0, 3, 7, 10];
-        var major5 = [0, 4, 7];
-        var minor5 = [0, 3, 7];
-        var minor5d = [0, 3, 6];
-        var augment = [0, 4, 8];
-        var chord2 = [0, 2, 7];
-        var startinvals1 = [4, -3, 8, -3, -1, 4];
-        var startinvals2 = [0, 7, 9, 5]
-        var startinvals3 = [0, 4, 5, 0]
-        var startinvals4 = [9, 2, 7, 0]
-        var startinvals5 = [2, 11, 7, 2, 9]
-        var chordprogression1 = [major5, major5, minor5, major5, major5, major5];
-        var chordprogression2 = [major5, major5, minor5, major5];
-        var chordprogression3 = [major5, major5, major5, major5];
-        var chordprogression4 = [minor5, minor5, major5, major5];
-        var chords = 
-            [[startinvals1, chordprogression1], 
-            [startinvals2, chordprogression2],
-            [startinvals3, chordprogression3],
-            [startinvals4, chordprogression4]]
-        var curchordprog = chords[Math.floor(Math.random()*chords.length)];
-        console.log("what");
-        console.log(curchordprog);
+        
+        
         for (repeat = 0; repeat < 5; repeat ++){
             var freqtotal = [];
+            var curchordprog = chords[Math.floor(Math.random()*chords.length)];
+            curmajorminor = curchordprog[2];
+
+            startingnnote = Math.floor(Math.random()*12)+1;
+            curmajorminor.push();
+            console.log("what");
+            console.log(curchordprog);
             for (k = 0; k < curchordprog[0].length; k ++){
                 tempval = []
+                startingnnote = Math.floor(Math.random()*12)+1;
                 for (k2 = 0; k2 < curchordprog[1][k].length; k2 ++){
+                    
+                    curchordprog[0][k][k2] += startingnnote;
                     console.log(midi[curchordprog[1][k][k2]+48+curchordprog[0][k]]);
                     tempval.push(midi[curchordprog[1][k][k2]+48+curchordprog[0][k]]);
                 }
                 freqtotal.push(tempval);   
             }
             console.log(freqtotal);
-            var notes = [major7, major7, minor7, dom7, major7, major7]
-            var freqtotal2 = [];
-            for (k = 0; k < curchordprog[0].length; k ++){
-                tempval = [];
-                for (k2 = 0; k2 < notes[k].length; k2++){
-                    tempval.push(midi[notes[k][k2]+60+curchordprog[0][k]]);
-                }
-                freqtotal2.push(tempval);
-            }
+            
+            // var freqtotal2 = [];
+            // for (k = 0; k < curchordprog[0].length; k ++){
+            //     tempval = [];
+            //     for (k2 = 0; k2 < notes[k].length; k2++){
+            //         tempval.push(midi[notes[k][k2]+60+curchordprog[0][k]]);
+            //     }
+            //     freqtotal2.push(tempval);
+            // }
             
             // var minor7  = [0, 3, 7, 10];
             // var freqs = [midi[67], midi[64], midi[60], midi[72]];
@@ -81,7 +97,7 @@ function makeSound(){
                 var oscs = [];
                 
                 if (checker){
-                        curfreq2 = freqtotal2[(j/2)%freqtotal2.length];
+                        curmajmin = curmajorminor[(j/2)%curmajorminor.length];
                         curfreq = freqtotal[(j/2)%freqtotal.length];
 
                         console.log((j/2)%freqtotal.length);       
@@ -103,27 +119,27 @@ function makeSound(){
                     nodes.filter = context.createBiquadFilter();
                     nodes.convolver = context.createConvolver();
                     nodes.volume = context.createGain();
-                    var gainNode = context.createGain();
+                    // var gainNode = context.createGain();
                     var oscillator = context.createOscillator();
                     oscillator.frequency.value = curfreq[x];
-                    // oscillator..value = 50;
+                    // oscillator..value = 50volume
                     oscillator.type = "sine"
-                    var biquadFilter = context.createBiquadFilter();
+                    // var biquadFilter = context.createBiquadFilter();
                     oscillator.connect(nodes.filter);
-                    oscillator.connect(nodes.convolver);
+                    // oscillator.connect(nodes.convolver);
                     nodes.filter.connect(nodes.volume);
                     nodes.volume.gain.value = 0.10;
                     // biquadFilter.connect(gainNode);
-                    // biquadFilter.type = "lowshelf";
+                    // nodes.filter.type = "lowshelf";
                     // biquadFilter.frequency.value = 100;
                     // biquadFilter.gain.value = 30;                 
                     nodes.volume.connect(context.destination);
-                    oscillator.start(((repeat * 24) +quickness*j).toFixed(5));
+                    oscillator.start((quickness*(repeat * 24 +j)).toFixed(9));
                     // gainNode = context.createGainNode();
                     // gainNode.connect(context.destination);
                     // oscillator.connect(gainNode);
                     // Connect the oscillator to our speakers
-                    oscillator.stop(((repeat * 24) +quickness*(j+.75)).toFixed(5));
+                    oscillator.stop((quickness*(repeat * 24 +j+.7)).toFixed(9));
                     oscs.push(oscillator);
                     // break;
                 }
@@ -135,13 +151,15 @@ function makeSound(){
                 compressor.attack.value = 0.001;
                 compressor.release.value = 0.001;
                 for (v = 0; v < 4; v ++){
+                    break;
                     var nodes = {};
                     nodes.filter = context.createBiquadFilter();
                     nodes.convolver = context.createConvolver();
                     nodes.volume = context.createGain();
                     var gainNode = context.createGain();
                     var oscillator = context.createOscillator();
-                    console.log(Math.floor(Math.random()*curfreq2.length));
+                    console.log(Math.floor(Math.random()*curmajorminor.length));
+
                     oscillator.frequency.value = curfreq2[Math.floor(Math.random()*curfreq2.length)];
                     // oscillator..value = 50;
                     oscillator.type = "sine";
@@ -183,4 +201,14 @@ function makeSound(){
         
         
         
+}
+
+function returnNotes(major, starting){
+    if (major){
+        notestopickfrom = majorscale;
+    }
+    else{
+        notestopickfrom = minorscale;
+    }
+    
 }
